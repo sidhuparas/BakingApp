07aa2a7ed2353a03facd60b0f69d36a5fa2f9d77
Paras Sidhu <sidhuparas1234@gmail.com>
2018-08-17 11:05:32 +0530

    Fragment and Models Boilerplate

---
+++ app/src/main/java/com/parassidhu/bakingapp/RecipeFragment.java
@@ -0,0 +1,74 @@
+package com.parassidhu.bakingapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class RecipeFragment extends Fragment {
+
+    @BindView(R.id.recipe_list) RecyclerView mRecipeList;
+
+    private OnFragmentInteractionListener mListener;
+    private Context context;
+    public RecipeFragment() { }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_recipe, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initialSetup();
+    }
+
+    private void initialSetup() {
+        context = getActivity();
+        mRecipeList.setLayoutManager(new GridLayoutManager(context, 2));
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
---
+++ app/src/main/java/com/parassidhu/bakingapp/models/Steps.java
@@ -0,0 +1,45 @@
+package com.parassidhu.bakingapp.models;
+
+public class Steps {
+    private int id;
+    private String shortDescription, description, videoURL;
+
+    public Steps(int id, String shortDescription, String description, String videoURL) {
+        this.id = id;
+        this.shortDescription = shortDescription;
+        this.description = description;
+        this.videoURL = videoURL;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVideoURL() {
+        return videoURL;
+    }
+
+    public void setVideoURL(String videoURL) {
+        this.videoURL = videoURL;
+    }
+}
